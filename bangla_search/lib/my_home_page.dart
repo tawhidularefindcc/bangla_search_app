24b33505c2c3e122d5bc180c8213c23add59d92c
Tawhidul Arefin <tawhidularefin530@gmail.com>
2023-01-31 23:51:49 +0600

    Initialized: the app

---
+++ bangla_search/lib/my_home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final data = [
+    "আমি",
+    "তুমি",
+    "সে",
+    "সোমা",
+    "তাঁর",
+    "আমাদের",
+  ];
+  var searchController = TextEditingController();
+  List<String> filteredData = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredData = data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: searchController,
+          onChanged: (value) {
+            setState(() {
+              filteredData = data
+                  .where((word) =>
+                      word.toLowerCase().contains(value.toLowerCase()))
+                  .toList();
+            });
+          },
+          decoration: const InputDecoration(
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search),
+              border: InputBorder.none),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: filteredData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(filteredData[index]),
+          );
+        },
+      ),
+    );
+  }
+}
